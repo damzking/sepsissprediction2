02fc2cad1b619c4dc1bdceec56e3dcbe9ac3b657
Victor Nyarko Anim <victor.nyarko@ymail.com>
2024-07-13 18:31:50 +0000

    app

--- src/myApp.py
+++ src/myApp.py
@@ -1,38 +1,3 @@
-import pandas as pd
-import numpy as np
-#from sklearn.base import BaseEstimator, TransformerMixin
-
-from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import ( 
-    LabelEncoder, 
-    RobustScaler, 
-)
-
-from imblearn.pipeline import Pipeline as ImbPipeline
-from imblearn.over_sampling import SMOTE
-from sklearn.compose import ColumnTransformer
-
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import ( 
-    GradientBoostingClassifier
-)
-
-
-from sklearn.naive_bayes import GaussianNB
-from xgboost import XGBClassifier
-from sklearn.linear_model import HuberRegressor
-
-from sklearn.metrics import (
-    accuracy_score, 
-    precision_score, 
-    recall_score, 
-    f1_score, 
-    roc_curve,
-    auc, 
-    confusion_matrix, 
-    classification_report
-)
 
 from fastapi import FastAPI
 import joblib
@@ -68,7 +33,7 @@ def MySepssisApi():
 
 GB_pipeline = joblib.load('../models/GB_pipeline.joblib')
 LogReg_pipeline = joblib.load('../models/LogReg_pipeline.joblib')
-#NB_pipeline = joblib.load('../models/NB_pipeline.joblib')
+SVM_pipeline = joblib.load('../models/SVM_pipeline.joblib')
 XGB_pipeline = joblib.load('../models/XGB_pipeline.joblib')
 encoder = joblib.load('../models/encoder.joblib')
 
